f9e785d8e3755516fde7e52e392d62a3bfeeb817
snailstudio2010 <1239749058@qq.com>
2019-09-18 11:03:10 +0800

    Add: project

---
+++ app/src/main/java/com/snailstudio2010/arms/MainActivity.java
@@ -0,0 +1,14 @@
+package com.snailstudio2010.arms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
