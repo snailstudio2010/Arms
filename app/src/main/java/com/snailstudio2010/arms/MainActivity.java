88a4598cb453ffc7e55cbfaf04764858b81d48cd
snailstudio2010 <1239749058@qq.com>
2019-09-18 14:08:40 +0800

    Fix

--- app/src/main/java/com/snailstudio2010/arms/MainActivity.java
+++ app/src/main/java/com/snailstudio2010/arms/MainActivity.java
@@ -1,8 +1,7 @@
 package com.snailstudio2010.arms;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
